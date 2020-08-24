de49bd06f3a2c14f24b21969e8c204119b0aa84c
Eduardo Vargas <evargast@adobe.com>
2020-08-24 11:13:18 -0500

    Reduce bundle size

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,39 @@
-
-import { afterAll, afterEach, beforeAll, jest } from './jest/actions';
 function mockRequestAnimationFrame(): void {
-    let offsetWidth: jest.SpyInstance<number, []>;
-    let offsetHeight: jest.SpyInstance<number, []>;
+  let offsetWidth: jest.SpyInstance<number, []>;
+  let offsetHeight: jest.SpyInstance<number, []>;
 
-    beforeAll(function () {
-        // ---- Give it a mock height and width so the frame can be rendered----
-        offsetWidth = jest
-            .spyOn(window.HTMLElement.prototype, "clientWidth", "get")
-            .mockImplementation(() => 1000);
+  beforeAll(function () {
+    // ---- Give it a mock height and width so the frame can be rendered----
+    offsetWidth = jest
+      .spyOn(window.HTMLElement.prototype, 'clientWidth', 'get')
+      .mockImplementation(() => 1000);
 
-        offsetHeight = jest
-            .spyOn(window.HTMLElement.prototype, "clientHeight", "get")
-            .mockImplementation(() => 1000);
+    offsetHeight = jest
+      .spyOn(window.HTMLElement.prototype, 'clientHeight', 'get')
+      .mockImplementation(() => 1000);
 
-        // ---------- Hijack & mock animation----------
-        window.HTMLElement.prototype.scrollIntoView = jest.fn();
-        jest.spyOn(
-            window,
-            "requestAnimationFrame",
-        ).mockImplementation(callback => setTimeout(callback, 0));
+    // ---------- Hijack & mock animation----------
+    window.HTMLElement.prototype.scrollIntoView = jest.fn();
+    jest
+      .spyOn(window, 'requestAnimationFrame')
+      .mockImplementation((callback) => setTimeout(callback, 0));
 
-        // ---------- Mock clock that animation uses for rendering----------
-        jest.useFakeTimers();
-    });
+    // ---------- Mock clock that animation uses for rendering----------
+    jest.useFakeTimers();
+  });
 
-    afterEach(() => {
-        /** Exhaust timers in case there are any animations
-         *  waiting to be rendered similar to using a waitFor() */
-        jest.runAllTimers();
-    });
+  afterEach(() => {
+    /** Exhaust timers in case there are any animations
+     *  waiting to be rendered similar to using a waitFor() */
+    jest.runAllTimers();
+  });
 
-    afterAll(function () {
-        /* Reset everything so all other tests can run without interference */
-        jest.useRealTimers();
-        offsetWidth.mockReset();
-        offsetHeight.mockReset();
-    });
+  afterAll(function () {
+    /* Reset everything so all other tests can run without interference */
+    jest.useRealTimers();
+    offsetWidth.mockReset();
+    offsetHeight.mockReset();
+  });
 }
 
-export default mockRequestAnimationFrame ;
+export default mockRequestAnimationFrame;
